2188eef7d151a107a497d7aa45a3d9fb8257060c
Charith Paranaliyanage <paranaliyanage@gmail.com>
2014-02-07 23:12:41 +0530

    Basic heat 1d program with out auto generated schedule

--- apps/stencil/heat_1d.cpp
+++ apps/stencil/heat_1d.cpp
@@ -3,29 +3,20 @@ using namespace Halide;
 
 int main(int argc, char **argv) {
 
-	ImageParam in_img(Float(32), 2);
-	Func heat_x("heat_x"), heat_y("heat_y");
+	ImageParam input(Float(32), 2);
 	Var x("x"), y("y");
 
-	Func input;
-	input(x, y) = in_img(clamp(x, 1, in_img.width() - 2),
-			clamp(y, 1, in_img.height() - 1));
+	Func heat("heat");
+	RDom r(1, input.width() - 2, 1, input.height() - 1);
 
-	// The algorithm
-	heat_x(x, y) = 0.125f
-			* (input(x - 1, y) - 2.0f * input(x, y) + input(x + 1, y));
-	heat_y(x, y) = heat_x(x, y - 1);
+	heat(x, y) = input(x, y);
+	heat(r.x, r.y) = 0.125f
+			* (heat(r.x - 1, r.y - 1) - 2.0f * heat(r.x, r.y - 1)
+					+ heat(r.x + 1, r.y - 1));
 
-	// The schedule
-	Halide::Var _x2, _x4, _y5;
-	input.reorder(x, y).reorder_storage(y, x).vectorize(x, 4).compute_at(heat_y,
-			_y5);
-	heat_x.split(x, x, _x2, 32).reorder(_x2, x, y).reorder_storage(x, y).vectorize(
-			_x2, 4).compute_at(heat_y, _y5);
-	heat_y.split(x, x, _x4, 32).split(y, y, _y5, 8).reorder(_x4, _y5, x, y).reorder_storage(
-			y, x).vectorize(_x4, 8).compute_root();
+	heat.compute_root();
 
-	heat_y.compile_to_file("heat_1d", in_img);
+	heat.compile_to_file("heat_1d", input);
 
 	return 0;
 }
--- apps/stencil/test.cpp
+++ apps/stencil/test.cpp
@@ -5,16 +5,10 @@
 #define TIMES 100
 struct timeval start, end;
 
-int print(Image<float> input, int row) {
-	if (row == -1) {
-		for (int y = 0; y < input.height(); y++) {
-			for (int x = 0; x < input.width(); x++) {
-				printf("%f\n", input(x, y));
-			}
-		}
-	} else {
+int print(Image<float> input) {
+	for (int y = 0; y < input.height(); y++) {
 		for (int x = 0; x < input.width(); x++) {
-			printf("%f\n", input(x, row));
+			printf("value in %d %d is %f\n", y, x, input(x, y));
 		}
 	}
 	return 0;
@@ -22,9 +16,7 @@ int print(Image<float> input, int row) {
 
 extern "C" {
 #include "heat_1d.h"
-}
-
-Image<float> heat_1d(Image<float> input) {
+}Image<float> halide_heat_1d(Image<float> input) {
 	Image<float> output(input.width(), input.height());
 	heat_1d(input, output);
 	gettimeofday(&start, 0);
@@ -46,28 +38,22 @@ int main(int argc, char **argv) {
 	printf("N_SIZE = %d, T_SIZE = %d\n", N_SIZE, T_SIZE);
 	Image<float> input = Image<float>(N_SIZE, T_SIZE);
 
-	for (int y = 0; y < input.height(); y++) {
-		for (int x = 0; x < input.width(); x++) {
-			if (y == 0) {
-				input(x, y) = 1.0 * (rand() % BASE);
-				//input(x, y) = 1.0 * (y * input.width() + x);
-			} else {
-				input(x, y) = 0;
-			}
-		}
+	for (int x = 0; x < input.width(); x++) {
+		input(x, 0) = 1.0 * (rand() % BASE);
+		//input(x, 0) = 1.0 * x;
+		input(x, 1) = 0;
 	}
 
-	print(input,0);
-	printf("Input is printed now \n");
+	//print(input);
 
-	Image<float> output = heat_1d(input);
+	Image<float> output = halide_heat_1d(input);
 
 	float halide_time = ((end.tv_sec - start.tv_sec)
 			+ (end.tv_usec - start.tv_usec) / 100000000.0f) / TIMES;
 
-	print(output,31);
+	//print(output);
 
-	printf("time is %f\n", halide_time);
+	printf("Halide consumed time : %f ms\n", halide_time);
 
 	return 0;
 }
