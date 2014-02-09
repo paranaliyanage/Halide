d043d5b754492e5cabe2d5923969a6cfdfa27cdd
Charith Paranaliyanage <paranaliyanage@gmail.com>
2014-02-09 16:14:04 +0530

    correct iteration limits

--- apps/stencil/heat_3d_np.cpp
+++ apps/stencil/heat_3d_np.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 	Var x("x"), y("y"), z("z"), w("w");
 
 	Func heat("heat");
-	RDom r(1, input.width() - 2, 1, input.height() - 2, 1, input.channels() - 1,
+	RDom r(1, input.width() - 2, 1, input.height() - 2, 1, input.channels() - 2,
 			1, input.extent(3) - 1);
 
 	heat(x, y, z, w) = input(x, y, z, w);
