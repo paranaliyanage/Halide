93ac45523a272a84c7ebe489a6962574cff1931d
Charith Paranaliyanage <paranaliyanage@gmail.com>
2014-02-27 14:04:24 +0530

    update correct schedule

--- apps/stencil/heat_2d_np_32768.cpp
+++ apps/stencil/heat_2d_np_32768.cpp
@@ -13,21 +13,34 @@ Func heat_step(Func input, Expr max_x, Expr max_y) {
 					* (clamped_0(x, y + 1) - 2.0f * clamped_0(x, y)
 							+ clamped_0(x, y - 1)) + clamped_0(x, y);
 
-	clamped_1(x,y) = heat_0(clamp(x, 1, max_x), clamp(y, 1, max_y));
-	heat_1(x,y) = 0.125f
+	clamped_1(x, y) = heat_0(clamp(x, 1, max_x), clamp(y, 1, max_y));
+	heat_1(x, y) = 0.125f
 			* (clamped_1(x + 1, y) - 2.0f * clamped_1(x, y)
 					+ clamped_1(x - 1, y))
 			+ 0.125f
 					* (clamped_1(x, y + 1) - 2.0f * clamped_1(x, y)
 							+ clamped_1(x, y - 1)) + clamped_1(x, y);
-	clamped_2(x,y) = heat_1(clamp(x, 1, max_x), clamp(y, 1, max_y));
-	heat_2(x,y) = 0.125f
+	clamped_2(x, y) = heat_1(clamp(x, 1, max_x), clamp(y, 1, max_y));
+	heat_2(x, y) = 0.125f
 			* (clamped_2(x + 1, y) - 2.0f * clamped_2(x, y)
 					+ clamped_2(x - 1, y))
 			+ 0.125f
 					* (clamped_2(x, y + 1) - 2.0f * clamped_2(x, y)
 							+ clamped_2(x, y - 1)) + clamped_2(x, y);
-	heat_2.compute_root();
+
+	Halide::Var _x2, _x6, _y7, _x10, _y11;
+	clamped_0.reorder(y, x).reorder_storage(y, x).compute_at(heat_1, y);
+	heat_0.split(x, x, _x2, 16).reorder(_x2, x, y).reorder_storage(y, x).vectorize(
+			_x2, 8).compute_at(heat_1, _y7);
+	clamped_1.reorder(y, x).reorder_storage(x, y).vectorize(y, 8).compute_at(
+			heat_1, _x6);
+	heat_1.split(x, x, _x6, 8).split(y, y, _y7, 4).reorder(_x6, _y7, x, y).reorder_storage(
+			x, y).vectorize(_x6, 2).parallel(y).compute_root();
+	clamped_2.reorder(x, y).reorder_storage(x, y).vectorize(x, 4).compute_at(
+			heat_2, x);
+	heat_2.split(x, x, _x10, 64).split(y, y, _y11, 4).reorder(_x10, _y11, x, y).reorder_storage(
+			y, x).vectorize(_x10, 8).parallel(y).compute_root();
+
 	return heat_2;
 
 }
@@ -40,7 +53,7 @@ int main(int argc, char **argv) {
 	Func heat[steps + 1];
 	heat[0](x, y) = input(x, y);
 	for (int t = 1; t <= macro_steps; t++) {
-		heat[t] = heat_step(heat[t - 1], input.width() - 2,input.height() - 2);
+		heat[t] = heat_step(heat[t - 1], input.width() - 2, input.height() - 2);
 	}
 	//heat[macro_steps].compute_root();
 
