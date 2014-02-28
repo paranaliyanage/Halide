aaa79ec47cbde5a8233b683f9fea364f00e2a60a
Charith Paranaliyanage <paranaliyanage@gmail.com>
2014-02-28 08:24:10 +0530

    completing 3d program as per new flow jonathan pointed out

--- apps/stencil/heat_3d_np.cpp
+++ apps/stencil/heat_3d_np.cpp
@@ -1,34 +1,65 @@
 #include <Halide.h>
 using namespace Halide;
 
+Func heat_step(Func input, Expr max_x, Expr max_y, Expr max_z) {
+	Var x("x"), y("y"), z("z");
+	Func heat_0("heat_0"), clamped_0("clamped_0"), heat_1("heat_1"), clamped_1(
+			"clamped_1"), heat_2("heat_0"), clamped_2("clamped_0");
+
+	clamped_0(x, y, z) = input(clamp(x, 1, max_x), clamp(y, 1, max_y),
+			clamp(z, 1, max_z));
+
+	heat_0(x, y, z) = 0.125f
+			* (clamped_0(x + 1, y, z) - 2.0f * clamped_0(x, y, z)
+					+ clamped_0(x - 1, y, z))
+			+ 0.125f
+					* (clamped_0(x, y + 1, z) - 2.0f * clamped_0(x, y, z)
+							+ clamped_0(x, y - 1, z))
+			+ 0.125f
+					* (clamped_0(x, y, z + 1) - 2.0f * clamped_0(x, y, z)
+							+ clamped_0(x, y, z - 1)) + clamped_0(x, y, z);
+
+	clamped_1(x, y, z) = heat_0(clamp(x, 1, max_x), clamp(y, 1, max_y),
+			clamp(z, 1, max_z));
+	heat_1(x, y, z) = 0.125f
+			* (clamped_1(x + 1, y, z) - 2.0f * clamped_1(x, y, z)
+					+ clamped_1(x - 1, y, z))
+			+ 0.125f
+					* (clamped_1(x, y + 1, z) - 2.0f * clamped_1(x, y, z)
+							+ clamped_1(x, y - 1, z))
+			+ 0.125f
+					* (clamped_1(x, y, z + 1) - 2.0f * clamped_1(x, y, z)
+							+ clamped_1(x, y, z - 1)) + clamped_1(x, y, z);
+
+	clamped_2(x, y, z) = heat_1(clamp(x, 1, max_x), clamp(y, 1, max_y),
+			clamp(z, 1, max_z));
+
+	heat_2(x, y, z) = 0.125f
+			* (clamped_2(x + 1, y, z) - 2.0f * clamped_2(x, y, z)
+					+ clamped_2(x - 1, y, z))
+			+ 0.125f
+					* (clamped_2(x, y + 1, z) - 2.0f * clamped_2(x, y, z)
+							+ clamped_2(x, y - 1, z))
+			+ 0.125f
+					* (clamped_2(x, y, z + 1) - 2.0f * clamped_2(x, y, z)
+							+ clamped_2(x, y, z - 1)) + clamped_2(x, y, z);
+
+	heat_2.compute_root();
+	return heat_2;
+}
+
 int main(int argc, char **argv) {
+	ImageParam input(Float(32), 3);
+	const unsigned int steps = 6;
+	const unsigned int macro_steps = steps / 3;
+	Var x("x"), y("y"), z("z");
+	Func heat[steps + 1];
+	heat[0](x, y, z) = input(x, y, z);
+	for (int t = 1; t <= macro_steps; t++) {
+		heat[t] = heat_step(heat[t - 1], input.width() - 2, input.height() - 2,
+				input.channels() - 2);
+	}
 
-	ImageParam input(Float(32), 4);
-	Var x("x"), y("y"), z("z"), w("w");
-
-	Func heat("heat");
-	RDom r(1, input.width() - 2, 1, input.height() - 2, 1, input.channels() - 2,
-			1, input.extent(3) - 1);
-
-	heat(x, y, z, w) = input(x, y, z, w);
-
-	heat(r.x, r.y, r.z, r.w) = 0.125f
-			* (heat(r.x + 1, r.y, r.z, r.w - 1)
-					- 2.0f * heat(r.x, r.y, r.z, r.w - 1)
-					+ heat(r.x - 1, r.y, r.z, r.w - 1))
-			+ 0.125f
-					* (heat(r.x, r.y + 1, r.z, r.w - 1)
-							- 2.0f * heat(r.x, r.y, r.z, r.w - 1)
-							+ heat(r.x, r.y - 1, r.z, r.w - 1))
-			+ 0.125f
-					* (heat(r.x, r.y, r.z + 1, r.w - 1)
-							- 2.0f * heat(r.x, r.y, r.z, r.w - 1)
-							+ heat(r.x, r.y, r.z - 1, r.w - 1))
-			+ heat(r.x, r.y, r.z, r.w - 1);
-
-	heat.compute_root();
-
-	heat.compile_to_file("heat_3d_np", input);
-
+	heat[macro_steps].compile_to_file("heat_3d_np", input);
 	return 0;
 }
--- apps/stencil/test_3d_np.cpp
+++ apps/stencil/test_3d_np.cpp
@@ -1,18 +1,16 @@
 #include <static_image.h>
 #include <image_io.h>
 #include <sys/time.h>
+#include <cmath>
 
 #define TIMES 20
 struct timeval start, end;
 
 int print(Image<float> input) {
-	for (int w = 0; w < input.size(3); w++) {
-		for (int z = 0; z < input.channels(); z++) {
-			for (int y = 0; y < input.height(); y++) {
-				for (int x = 0; x < input.width(); x++) {
-					printf("value in %d %d %d %d is %f\n", w, y, x, z,
-							input(x, y, z, w));
-				}
+	for (int z = 0; z < input.channels(); z++) {
+		for (int y = 0; y < input.height(); y++) {
+			for (int x = 0; x < input.width(); x++) {
+				printf("value in %d %d %d is %f\n", z, y, x, input(x, y, z));
 			}
 		}
 	}
@@ -23,53 +21,51 @@ extern "C" {
 #include "heat_3d_np.h"
 }
 Image<float> halide_heat_3d(Image<float> input) {
-	Image<float> output(input.width(), input.height(), input.channels(),
-			input.size(3));
+	Image<float> output(input.width(), input.height(), input.channels());
 	heat_3d_np(input, output);
-	gettimeofday(&start, 0);
+	float halide_time = std::numeric_limits<float>::max();
 	for (int i = 0; i < TIMES; ++i) {
+		gettimeofday(&start, 0);
 		heat_3d_np(input, output);
+		gettimeofday(&end, 0);
+		halide_time = std::min(halide_time,
+				((end.tv_sec - start.tv_sec)
+						+ (end.tv_usec - start.tv_usec) / 100000000.0f));
+
 	}
-	gettimeofday(&end, 0);
+	printf("Halide consumed time : %f ms\n", halide_time);
 	return output;
 }
 
 int main(int argc, char **argv) {
 	const int BASE = 1024;
-	if (argc < 3) {
-		printf("argc < 3, quit! \n");
+	if (argc < 2) {
+		printf("argc < 2, quit! \n");
 		exit(1);
 	}
 	int N_SIZE = atoi(argv[1]);
-	int T_SIZE = atoi(argv[2]);
-	printf("N_SIZE = %d, T_SIZE = %d\n", N_SIZE, T_SIZE);
-	Image<float> input = Image<float>(N_SIZE, N_SIZE, N_SIZE, T_SIZE);
+	printf("N_SIZE = %d \n", N_SIZE);
+	Image<float> input = Image<float>(N_SIZE, N_SIZE, N_SIZE);
 
-	for (int x = 0; x < input.width(); ++x) {
-		for (int y = 0; y < input.height(); ++y) {
-			for (int z = 0; z < input.channels(); ++z) {
+	for (int z = 0; z < input.channels(); z++) {
+		for (int y = 0; y < input.height(); y++) {
+			for (int x = 0; x < input.width(); x++) {
 				if (x == 0 || x == input.width() - 1 || y == 0
 						|| y == input.height() - 1 || z == 0
 						|| z == input.channels() - 1) {
-					input(x, y, z, 0) = 0;
+					input(x, y, z) = 0;
 				} else {
-					input(x, y, z, 0) = 1.0 * (rand() % BASE);
+					input(x, y, z) = 1.0 * (rand() % BASE);
 				}
-				input(x, y, z, 1) = 0;
+				input(x, y, z) = 0;
 			}
 		}
 	}
-
 	//print(input);
 
 	Image<float> output = halide_heat_3d(input);
 
-	float halide_time = ((end.tv_sec - start.tv_sec)
-			+ (end.tv_usec - start.tv_usec) / 100000000.0f) / TIMES;
-
 	//print(output);
-
-	printf("Halide consumed time : %f ms\n", halide_time);
 
 	return 0;
 }
